2aaca89a24df58014e81c1489067992cf9882624
TanyaShved <tanya.shved.89@gmail.com>
2021-03-18 17:40:42 +0200

    Remove alphanumeric validation of name

--- routes/api/validation.js
+++ routes/api/validation.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 const { HttpCode } = require('../../helpers/constants')
 
 const schemaCreateComment = Joi.object({
-    name: Joi.string().alphanum().min(3).max(30).required(),
+    name: Joi.string().min(3).max(30).required(),
     comment: Joi.string().min(1).max(250).required(),
 })
 
